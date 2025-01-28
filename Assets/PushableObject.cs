a62493cafe67612e04efb80b9ec67af4c1b88679
TurtleHeIIMax <maxstinkyfoot@gmail.com>
2025-01-28 15:51:41 -0800

    Basically finished push / pullable objects

---
+++ Assets/PushableObject.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushableObject : MonoBehaviour
+{
+    public int stationaryLayer = 11;
+
+    public LayerMask playerLayer;
+    private GroundCheck groundCheck;
+    private PuzzlePlayerController playerScript;
+    private Rigidbody RB;
+    private bool falling = false;
+    private bool pushable = true;
+
+    void Start()
+    {
+        playerScript = GameObject.FindWithTag("Player").GetComponent<PuzzlePlayerController>();
+        groundCheck = transform.GetChild(0).gameObject.GetComponent<GroundCheck>();
+        RB = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (transform.parent == null)
+            gameObject.layer = stationaryLayer;
+
+        if (!groundCheck.isGrounded && !falling)
+        {
+            if (playerScript.pushingObject == transform)
+                playerScript.EndPush();
+            RB.useGravity = true;
+            falling = true;
+            pushable = false;
+        }
+        else if (groundCheck.isGrounded && falling)
+        {
+            RB.useGravity = false;
+            falling = false;
+            pushable = true;
+        }
+        groundCheck.isGrounded = false;
+    }
+
+    public void OnTriggerEnter(Collider col)
+    {
+        PushCheck(col);
+    }
+
+    public void OnTriggerStay(Collider col)
+    {
+        PushCheck(col);
+    }
+
+    void PushCheck(Collider col)
+    {
+        if (!pushable)
+            return;
+        if (playerLayer == (playerLayer | (1 << col.gameObject.layer)))
+        {
+            if (playerScript != null && playerScript.interactInput && playerScript.pushingObject == null)
+            {
+                Vector3 pushDirection = playerScript.transform.position - transform.position;
+                pushDirection = transform.rotation * pushDirection;
+
+                playerScript.TryPushing(transform, Mathf.Abs(pushDirection.x) > Mathf.Abs(pushDirection.z));
+            }
+        }
+    }
+}
--- Assets/Scripts/Player/PuzzlePlayerController.cs
+++ Assets/Scripts/Player/PuzzlePlayerController.cs
@@ -24,6 +24,13 @@ public class PuzzlePlayerController : MonoBehaviour
     public float invulnerabilityTime = 0.25f;
     public float respawnInvulnerabilityTime = 2.0f;
 
+    public bool interactInput = false;
+    private bool usedInteractInput = false;
+
+    public Transform pushingObject = null;
+    public bool pushDir;
+    public int pushingLayer = 12;
+
     void Start()
     {
         curHealth = maxHealth;
@@ -33,7 +40,57 @@ public class PuzzlePlayerController : MonoBehaviour
 
     void Update()
     {
+        interactInput = false;
+
+        if (Input.GetButton("Jab"))
+            interactInput = true;
+        else
+            usedInteractInput = false;
+
         normalizedInputs = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        if (pushingObject != null)
+        {
+            if (!interactInput)
+            {
+                EndPush();
+                return;
+            }
+
+            if (pushDir)
+            {
+                if (normalizedInputs.x < 0)
+                {
+                    normalizedInputs.x = Mathf.Clamp(normalizedInputs.x, -1, 0);
+                    normalizedInputs.y = Mathf.Clamp(normalizedInputs.y, -1, 0);
+                }
+                else
+                {
+                    normalizedInputs.x = Mathf.Clamp(normalizedInputs.x, 0, 1);
+                    normalizedInputs.y = Mathf.Clamp(normalizedInputs.y, 0, 1);
+                }
+            }
+            else
+            {
+                if (normalizedInputs.x < 0)
+                {
+                    normalizedInputs.x = Mathf.Clamp(normalizedInputs.x, -1, 0);
+                    normalizedInputs.y = Mathf.Clamp(normalizedInputs.y, 0, 1);
+                }
+                else
+                {
+                    normalizedInputs.x = Mathf.Clamp(normalizedInputs.x, 0, 1);
+                    normalizedInputs.y = Mathf.Clamp(normalizedInputs.y, -1, 0);
+                }
+            }
+
+            float minDir = 0;
+            if (Mathf.Abs(normalizedInputs.x) < Mathf.Abs(normalizedInputs.y))
+                minDir = Mathf.Abs(normalizedInputs.x);
+            else
+                minDir = Mathf.Abs(normalizedInputs.y);
+            normalizedInputs.x = Mathf.Clamp(normalizedInputs.x, -minDir, minDir);
+            normalizedInputs.y = Mathf.Clamp(normalizedInputs.y, -minDir, minDir);
+        }
     }
 
     void FixedUpdate()
@@ -46,10 +103,23 @@ public class PuzzlePlayerController : MonoBehaviour
             if (Input.GetButton("Crouch"))
                 isCrouched = true;
             RB.velocity = new Vector3(normalizedInputs.x * playerSpeed.x * Time.fixedDeltaTime, RB.velocity.y, normalizedInputs.y * playerSpeed.y * Time.fixedDeltaTime);
+
             if (normalizedInputs.x > 0)
+            {
+                if (pushingObject != null)
+                    pushingObject.transform.parent = null;
                 transform.localScale = new Vector3(-1f, 1f, 1f);
+                if (pushingObject != null)
+                    pushingObject.transform.parent = transform;
+            }
             else if (normalizedInputs.x < 0)
+            {
+                if (pushingObject != null)
+                    pushingObject.transform.parent = null;
                 transform.localScale = new Vector3(1f, 1f, 1f);
+                if (pushingObject != null)
+                    pushingObject.transform.parent = transform;
+            }
         }
 
         playerAnimator.SetBool("InAir", !groundChecker.isGrounded);
@@ -96,5 +166,20 @@ public class PuzzlePlayerController : MonoBehaviour
         invulnerable = true;
         yield return new WaitForSeconds(time);
         invulnerable = false;
+    }
+
+    public void EndPush()
+    {
+        pushingObject.transform.parent = null;
+        pushingObject = null;
+    }
+
+    public void TryPushing(Transform newObj, bool PushDir)
+    {
+        usedInteractInput = true;
+        pushingObject = newObj;
+        newObj.transform.parent = transform;
+        newObj.gameObject.layer = pushingLayer;
+        pushDir = PushDir;
     }
 }
